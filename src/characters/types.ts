a0e72b58599b87bab27adeb6090e175d48b44ff0
LizzieCamp <55543295+LizzieCamp@users.noreply.github.com>
2022-11-17 10:10:51 +0000

    feat: get basic fight working (#4)

--- src/characters/types.ts
+++ src/characters/types.ts
@@ -5,24 +5,23 @@ type BaseCharacter = {
 };
 
 type Lightweight = BaseCharacter & {
-    category: Extract<CharacterCategory, 'lightweight'>;
-    agility: 1.25; // dodge/attack rate
-    hp: 75;
+    attackCooldown: 8;
+    maxHP: 75;
     damage: 10;
 };
 
 type Middleweight = BaseCharacter & {
-    category: Extract<CharacterCategory, 'middleweight'>;
-    agility: 1; // dodge/attack rate
-    hp: 100;
+    attackCooldown: 10;
+    maxHP: 100;
     damage: 15;
 };
 
 type Heavyweight = BaseCharacter & {
-    category: Extract<CharacterCategory, 'heavyweight'>;
-    agility: 0.75; // dodge/attack rate
-    hp: 125;
+    attackCooldown: 13;
+    maxHP: 125;
     damage: 20;
 };
 
-export type Character = Lightweight | Middleweight | Heavyweight;
+export type CharacterAttributes = Lightweight | Middleweight | Heavyweight;
+
+
